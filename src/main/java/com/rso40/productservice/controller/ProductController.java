84c2166e384436e0bd361d1f93e688b826f22fa0
anjabrelih <ab0555@student.uni-lj.si>
2023-01-11 23:48:19 +0100

    ui

--- src/main/java/com/rso40/productservice/controller/ProductController.java
+++ src/main/java/com/rso40/productservice/controller/ProductController.java
@@ -15,25 +15,50 @@ import com.rso40.productservice.dto.ProductResponse;
 import com.rso40.productservice.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("product")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
     
     //End point
-    @PostMapping
+    @PostMapping("/post-product")
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest){
+    public ModelAndView createProduct(ProductRequest productRequest){
+        System.out.println("Post mapping");
+        System.out.println(productRequest);
         productService.createProduct(productRequest);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new-product");
+        return modelAndView;
     }
 
-    //End point
-    @GetMapping
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView getAllProd() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("product_list");
+        List<ProductResponse> listProducts = productService.getAllProducts();
+        modelAndView.getModelMap().addAttribute("listProducts",listProducts);
+        return modelAndView;
+    }
+
+    @GetMapping("/products")
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts(){
         return productService.getAllProducts();
     }
+
+    @GetMapping("/new-product")
+    public ModelAndView addNewProduct(ProductRequest productRequest){
+        System.out.println("Get mapping");
+        System.out.println(productRequest);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new_product_form");
+        return modelAndView;
+    }
+
 }
